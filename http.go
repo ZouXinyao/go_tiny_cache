63a3cb6d04a558951a48eb1e9ae66db105839f5b
zouxinyao <zouxinyao@outlook.com>
2021-07-17 17:05:38 +0800

    分布式缓存：适配之前的功能

--- http.go
+++ http.go
@@ -2,7 +2,9 @@ package go_tiny_cache
 
 import (
 	"fmt"
+	"github.com/golang/protobuf/proto"
 	"go_tiny_cache/consistenthash"
+	pb "go_tiny_cache/tinycachepb"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -67,6 +69,15 @@ func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/octet-stream")
 	w.Write(view.ByteSlice())
+
+	// Write the value to the response body as a proto message.
+	body, err := proto.Marshal(&pb.Response{Value: view.ByteSlice()})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Write(body)
 }
 
 func (p *HTTPPool) Set(peers ...string) {
@@ -97,28 +108,31 @@ type httpGetter struct {
 	baseURL string
 }
 
-func (h *httpGetter) Get(group string, key string) ([]byte, error) {
+func (h *httpGetter) Get(in *pb.Request, out *pb.Response) error {
 	u := fmt.Sprintf(
 		"%v%v/%v",
 		h.baseURL,
-		url.QueryEscape(group),
-		url.QueryEscape(key),
+		url.QueryEscape(in.GetGroup()),
+		url.QueryEscape(in.GetKey()),
 	)
 
 	res, err := http.Get(u)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	defer res.Body.Close()
 
 	if res.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("server returned: %v", res.Status)
+		return fmt.Errorf("server returned: %v", res.Status)
 	}
 
 	bytes, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		return nil, fmt.Errorf("reading Response body: %v", err)
+		return fmt.Errorf("reading Response body: %v", err)
 	}
 
-	return bytes, nil
+	if err = proto.Unmarshal(bytes, out); err != nil {
+		return fmt.Errorf("decoding response body: %v", err)
+	}
+	return nil
 }
--- peer.go
+++ peer.go
@@ -1,5 +1,7 @@
 package go_tiny_cache
 
+import pb "go_tiny_cache/tinycachepb"
+
 // 根据传入的key值选择相应的节点，理解为客户端？
 type PeerPicker interface {
 	PickPeer(key string) (peer PeerGetter, ok bool)
@@ -7,5 +9,5 @@ type PeerPicker interface {
 
 // Get方法用于从对应的group查找缓存值。
 type PeerGetter interface {
-	Get(group string, key string) ([]byte, error)
+	Get(in *pb.Request, out *pb.Response) error
 }
